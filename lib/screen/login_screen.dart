2b710be9be03155fa273e09a80fd2954465facb0
Yael <brauliomcarrillo@gmail.com>
2026-02-26 08:47:45 -0600

    feat(rive) animation added

---
+++ lib/screen/login_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //evita nudge o cámaras frontales móviles
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: RiveAnimation.asset('animated_login_character.riv'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
